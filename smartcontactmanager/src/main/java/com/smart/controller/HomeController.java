599a7084b06e63588067e1b5512b1aeb6af5ff2e
Ankit <ankitpal28090016@gmail.com>
2023-08-11 16:49:18 +0530

    Creating Entity and connecting with mysql database

---
+++ smartcontactmanager/src/main/java/com/smart/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.smart.controller;
+
+import com.smart.dao.UserRepository;
+import com.smart.entities.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+
+}
---
+++ smartcontactmanager/src/main/java/com/smart/dao/UserRepository.java
@@ -0,0 +1,10 @@
+package com.smart.dao;
+
+
+import com.smart.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+
+}
