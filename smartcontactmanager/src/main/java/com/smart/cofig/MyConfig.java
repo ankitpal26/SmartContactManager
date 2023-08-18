f5f924f018f75bfa6844e97bb9ab86621a0862dd
Ankit <ankitpal28090016@gmail.com>
2023-08-18 21:58:24 +0530

    2 steps of spring security completed

--- smartcontactmanager/src/main/java/com/smart/cofig/MyConfig.java
+++ smartcontactmanager/src/main/java/com/smart/cofig/MyConfig.java
@@ -1,10 +1,41 @@
 package com.smart.cofig;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
-public class MyConfig {
+@EnableWebSecurity
+public class MyConfig  {
 
+    @Bean
+    public UserDetailsService getUserDetailsService()
+    {
+        return new UserDetailsServiceImp();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder()
+    {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider()
+    {
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+
+        daoAuthenticationProvider.setUserDetailsService(this.getUserDetailsService());
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+
+        return daoAuthenticationProvider;
+    }
+
+// configure method...
 
 
 }
