108c8469af5fe92fb89b639a19daf5b2a295829e
Ankit <ankitpal28090016@gmail.com>
2023-08-22 13:57:16 +0530

    adding conguration of accessible apis

--- smartcontactmanager/src/main/java/com/smart/cofig/MyConfig.java
+++ smartcontactmanager/src/main/java/com/smart/cofig/MyConfig.java
@@ -2,14 +2,16 @@ package com.smart.cofig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
-public class MyConfig extends WebSecurityConfiguration {
+public class MyConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
     public UserDetailsService getUserDetailsService()
@@ -34,7 +36,18 @@ public class MyConfig extends WebSecurityConfiguration {
         return daoAuthenticationProvider;
     }
 
+
 // configure method...
 
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(authenticationProvider());
+    }
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/admin/**").hasRole("ADMIN")
+                .antMatchers("/user/**").hasRole("USER")
+                .antMatchers("/**").permitAll().and().formLogin().and().csrf().disable();
+    }
 }
