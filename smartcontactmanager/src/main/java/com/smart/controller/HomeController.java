06701f1a1b5e468033c9cd8614e15b14fd0fd9fc
Ankit <ankitpal28090016@gmail.com>
2023-08-23 23:24:18 +0530

    Customize login page

--- smartcontactmanager/src/main/java/com/smart/controller/HomeController.java
+++ smartcontactmanager/src/main/java/com/smart/controller/HomeController.java
@@ -10,10 +10,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class HomeController {
@@ -101,4 +98,14 @@ public class HomeController {
 
 
     }
+
+    //handler for custom login
+
+    @GetMapping("/signin")
+    public String customLogin(Model model)
+    {
+        model.addAttribute("title","Login Page !!");
+        return "login";
+    }
+
 }
