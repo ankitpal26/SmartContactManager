56be5969ca99bfc954afd90a31f9435f03c986ee
Ankit <ankitpal28090016@gmail.com>
2023-08-28 23:16:54 +0530

    Creating User Dashboard

--- smartcontactmanager/src/main/java/com/smart/controller/UserController.java
+++ smartcontactmanager/src/main/java/com/smart/controller/UserController.java
@@ -1,14 +1,35 @@
 package com.smart.controller;
 
+import com.smart.dao.UserRepository;
+import com.smart.entities.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
 
+    @Autowired
+    private UserRepository userRepository;
+
     @RequestMapping("/index")
-    public String dashboard() {
+    public String dashboard(Model model, Principal principal)
+    {
+        String userName = principal.getName();
+        System.out.println("USERNAME" +userName);
+
+        //get the user using username(Email)
+
+        User user = userRepository.getUserByUserName(userName);
+        System.out.println("USER " +user);
+
+        model.addAttribute("user",user);
+
+
 
         return "normal/user_dashboard";
     }
